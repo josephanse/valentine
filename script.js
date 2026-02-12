07420e9be2a6be55a76b1acbe1945633be2a7e85
josephanse <90273160+josephanse@users.noreply.github.com>
2026-02-12 17:42:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
-const response = document.getElementById("response");
-const music = document.getElementById("music");
 
-// Make "No" button run away
 noBtn.addEventListener("mouseover", () => {
 const x = Math.random() * (window.innerWidth - 150);
 const y = Math.random() * (window.innerHeight - 100);
@@ -11,21 +8,6 @@ noBtn.style.left = `${x}px`;
 noBtn.style.top = `${y}px`;
 });
 
-// When she clicks YES
 yesBtn.addEventListener("click", () => {
-response.classList.remove("hidden");
-music.play();
-createHearts();
+window.location.href = "yes.html";
 });
-
-// Heart animation
-function createHearts() {
-for (let i = 0; i < 60; i++) {
-const heart = document.createElement("div");
-heart.classList.add("heart");
-heart.innerHTML = "💖";
-heart.style.left = Math.random() * 100 + "vw";
-heart.style.animationDuration = (Math.random() * 3 + 2) + "s";
-document.body.appendChild(heart);
-}
-}
