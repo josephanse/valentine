000251a9373c65674e52772015e8a8a0a2dc641e
josephanse <90273160+josephanse@users.noreply.github.com>
2026-02-12 17:51:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 
 noBtn.addEventListener("mouseover", () => {
-const x = Math.random() * (window.innerWidth - 150);
-const y = Math.random() * (window.innerHeight - 100);
-noBtn.style.left = `${x}px`;
-noBtn.style.top = `${y}px`;
+    const x = Math.random() * (window.innerWidth - 150);
+    const y = Math.random() * (window.innerHeight - 100);
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
 });
 
 yesBtn.addEventListener("click", () => {
-window.location.href = "yes.html";
+    window.location.href = "yes.html";
 });
